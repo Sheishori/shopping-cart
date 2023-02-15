deadd2f6b6817d3665df28b92ba8f1ac698a54fd
Sheishori <sheishori@gmail.com>
2023-02-15 16:17:43 +0100

    Move RouteSwitch to the components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import RouteSwitch from './RouteSwitch';
+import RouteSwitch from './components/RouteSwitch';
 import './styles/App.css';
 
 function App() {
--- src/components/RouteSwitch.tsx
+++ src/components/RouteSwitch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Catalog from './components/Catalog';
+import Home from './Home';
+import Catalog from './Catalog';
 
 function RouteSwitch() {
 	return (
