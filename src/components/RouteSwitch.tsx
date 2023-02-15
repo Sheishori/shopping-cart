b2544a21e431758bb0c1a1772882c97769b4d387
Sheishori <sheishori@gmail.com>
2023-02-15 16:20:39 +0100

    Add Cart to Routes

--- src/components/RouteSwitch.tsx
+++ src/components/RouteSwitch.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Catalog from './Catalog';
+import Cart from './Cart';
 
 function RouteSwitch() {
 	return (
@@ -9,6 +10,7 @@ function RouteSwitch() {
 			<Routes>
 				<Route path='/' element={<Home />} />
 				<Route path='/products' element={<Catalog />} />
+				<Route path='/cart' element={<Cart />} />
 			</Routes>
 		</BrowserRouter>
 	);
