29fbb0eb57975a4bccc6e8b705ff01150b9fe729
Sheishori <sheishori@gmail.com>
2023-02-15 13:40:43 +0100

    Add empty Header, Main and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-	return <div id='App'></div>;
+	return (
+		<div id='App'>
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
