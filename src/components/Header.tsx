2b6d4735c8da9ad51812d31a873445f30c4fd0bc
Sheishori <sheishori@gmail.com>
2023-02-15 14:42:15 +0100

    Add a Navigation component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 function Header() {
-	return <header></header>;
+	return (
+		<header>
+			<Navigation />
+		</header>
+	);
 }
 
 export default Header;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navigation() {
+	return (
+		<div>
+			<ul>
+				<li>Home</li>
+				<li>Products</li>
+				<li>Cart</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Navigation;
