385ff2811bd6fbd61da34c21f21cbdbe1460ed21
Sheishori <sheishori@gmail.com>
2023-02-16 16:29:10 +0100

    Fix test

--- src/__tests__/Products.test.tsx
+++ src/__tests__/Products.test.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { ReactComponentElement, ReactFragment, ReactNode } from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserEvent from '@testing-library/user-event';
 import Products from '../components/Products';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
 
 describe('Products component', () => {
 	it('renders a products list', () => {
-		render(<Products />);
+		const mockStore = configureStore();
+		const store = mockStore({ products: [] });
+
+		const providers = ({ children }: React.PropsWithChildren<unknown>) => {
+			return <Provider store={store}>{children}</Provider>;
+		};
+
+		render(<Products />, { wrapper: providers });
 		expect(screen.getByRole('list')).toBeInTheDocument();
 	});
 });
