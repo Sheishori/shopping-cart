1ab3bc8f0c3e4cc887b06863de9605d3151b6600
Sheishori <sheishori@gmail.com>
2023-02-17 12:35:21 +0100

    Render product information on the product page

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -9,6 +9,7 @@ function ProductDetail() {
 	const productId = Number(useParams().id);
 	let product = products.find((item: Product) => item.id === productId);
 	const [productDetail, setproductDetail] = useState({});
+	const [initializing, setInitializing] = useState(true);
 
 	useEffect(() => {
 		async function setProduct(product: Product | undefined, productId: number) {
@@ -18,11 +19,30 @@ function ProductDetail() {
 			}
 			setproductDetail(product);
 		}
-
 		setProduct(product, productId);
+		setInitializing(false);
 	}, []);
 
-	return <div id='product'></div>;
+	function init(initializing: boolean, product: any) {
+		if (initializing) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className='details'>
+					<h3>{product.title}</h3>
+					<div className='image'>
+						<img src={product.image} alt={product.title} />
+					</div>
+					<div className='info'>
+						<div className='desc'>{product.description}</div>
+						<div className='price'>${product.price}</div>
+					</div>
+				</div>
+			);
+		}
+	}
+
+	return <div id='product'>{init(initializing, productDetail)}</div>;
 }
 
 export default ProductDetail;
