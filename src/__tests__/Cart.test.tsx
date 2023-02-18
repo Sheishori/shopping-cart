ca2985423386e9facc2962e7c21302cecf5cd45c
Sheishori <sheishori@gmail.com>
2023-02-18 18:35:01 +0100

    Add test file to cart

---
+++ src/__tests__/Cart.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import UserEvent from '@testing-library/user-event';
+import Cart from '../components/Cart';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+describe('Cart component', () => {
+	it('notifies if the cart is empty', () => {
+		const mockStore = configureStore();
+		const store = mockStore({ products: [] });
+
+		const providers = ({ children }: React.PropsWithChildren<unknown>) => {
+			return <Provider store={store}>{children}</Provider>;
+		};
+
+		render(<Cart />, { wrapper: providers });
+		expect(screen.getByRole('dialog').textContent).toMatch(
+			'Your cart is empty!'
+		);
+	});
+
+	it("doesn't show 'empty' notification if cart has items", () => {
+		const mockStore = configureStore();
+		const store = mockStore({
+			products: [
+				{
+					id: '1',
+					title: 'cotton shirt ',
+					quantity: 1,
+				},
+			],
+		});
+
+		const providers = ({ children }: React.PropsWithChildren<unknown>) => {
+			return <Provider store={store}>{children}</Provider>;
+		};
+
+		render(<Cart />, { wrapper: providers });
+		expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+	});
+});
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { stat } from 'fs';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import '../styles/Cart.css';
 
 function Cart() {
+	const cartContents = useSelector((state: any) => state.cart);
+
+	useEffect(() => {
+		console.log(cartContents);
+	}, []);
+
 	return (
 		<main id='cart'>
 			<h3>Your cart:</h3>
 			<div className='contents'>
-				<div className='left'>Your cart is empty!</div>
+				<div className='left'>
+					<p role='dialog'>Your cart is empty!</p>
+				</div>
 				<div className='right'>
 					<p>Total:</p>
 					<button id='checkout'>Checkout</button>
