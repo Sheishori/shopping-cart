115de4ba7e2cfe88b8aad8868bc3ed0f86e78110
Sheishori <sheishori@gmail.com>
2023-02-18 18:18:04 +0100

    Add base elements to the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../styles/Cart.css';
 
 function Cart() {
 	return (
 		<main id='cart'>
-			<h3>Cart:</h3>
+			<h3>Your cart:</h3>
+			<div className='contents'>
+				<div className='left'>Your cart is empty!</div>
+				<div className='right'>
+					<p>Total:</p>
+					<button id='checkout'>Checkout</button>
+				</div>
+			</div>
+			<button className='back'>Go back</button>
 		</main>
 	);
 }
