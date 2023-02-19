f9587891cda1ac5ae01056d842ebe4eb226919bb
Sheishori <sheishori@gmail.com>
2023-02-19 18:44:27 +0100

    Add missing actions

--- src/components/state/actions.tsx
+++ src/components/state/actions.tsx
@@ -1,4 +1,9 @@
-import { ADD_TO_CART, SET_PRODUCTS } from './actionTypes';
+import {
+	ADD_TO_CART,
+	REMOVE_FROM_CART,
+	SET_PRODUCTS,
+	UPDATE_QUANTITY,
+} from './actionTypes';
 import { Product } from './types';
 
 export function setProductData(data: any) {
@@ -23,4 +28,27 @@ export function addProductToCart(product: any, quantity: number) {
 	};
 
 	return addProduct;
+}
+
+export function updateQuantityInCart(product: any, quantity: number) {
+	const updateQuantity = {
+		type: UPDATE_QUANTITY,
+		payload: {
+			id: product.id,
+			quantity: quantity,
+		},
+	};
+
+	return updateQuantity;
+}
+
+export function removeFromCart(product: any) {
+	const removeProduct = {
+		type: REMOVE_FROM_CART,
+		payload: {
+			id: product.id,
+		},
+	};
+
+	return removeProduct;
 }
