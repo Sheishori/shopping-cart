c81a5414c79ef8dc074d6dec8ba1222032bfe46a
Sheishori <sheishori@gmail.com>
2023-02-19 18:52:59 +0100

    Refactor actions

--- src/components/state/actions.tsx
+++ src/components/state/actions.tsx
@@ -6,10 +6,10 @@ import {
 } from './actionTypes';
 import { Product } from './types';
 
-export function setProductData(data: any) {
+export function setProductData(productData: any) {
 	const setProducts = {
 		type: SET_PRODUCTS,
-		productData: data,
+		productData,
 	};
 
 	return setProducts;
@@ -22,31 +22,31 @@ export function addProductToCart(product: any, quantity: number) {
 			id: product.id,
 			title: product.title,
 			price: product.price,
-			quantity: quantity,
 			image: product.image,
+			quantity,
 		},
 	};
 
 	return addProduct;
 }
 
-export function updateQuantityInCart(product: any, quantity: number) {
+export function updateQuantityInCart(id: number, quantity: number) {
 	const updateQuantity = {
 		type: UPDATE_QUANTITY,
 		payload: {
-			id: product.id,
-			quantity: quantity,
+			id,
+			quantity,
 		},
 	};
 
 	return updateQuantity;
 }
 
-export function removeFromCart(product: any) {
+export function removeFromCart(id: number) {
 	const removeProduct = {
 		type: REMOVE_FROM_CART,
 		payload: {
-			id: product.id,
+			id,
 		},
 	};
 
