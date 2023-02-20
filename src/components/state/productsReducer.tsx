253f0279791421524a7d07bda9f56ba6baf96a3f
Sheishori <sheishori@gmail.com>
2023-02-20 15:23:52 +0100

    Add type to state in productsReducer

--- src/components/state/productsReducer.tsx
+++ src/components/state/productsReducer.tsx
@@ -1,6 +1,7 @@
 import { SET_PRODUCTS } from './actionTypes';
+import { Product } from './types';
 
-export default function productsReducer(state = [], action: any) {
+export default function productsReducer(state: Product[] = [], action: any) {
 	switch (action.type) {
 		case SET_PRODUCTS:
 			return action.productData;
